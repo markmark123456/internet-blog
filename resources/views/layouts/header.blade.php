d5c7b0cb65c891351cb03d0bd7a1084baeeb602a
admin <markmark0489291@gmail.com>
2025-09-18 19:14:59 +0600

    header fix

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,24 +6,21 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
-        {{-- Кнопки для авторизованного пользователя --}}
-        @auth
-            <form action="{{ route('logout') }}" method="POST" class="d-inline">
-                @csrf
-                <button type="submit" class="btn btn-danger mb-3">Выйти</button>
-            </form>
-        @endauth
-
-        {{-- Кнопки для гостя --}}
-        @guest
-            <a href="{{ route('login.show') }}" class="btn btn-primary mb-3">Войти</a>
-            <a href="{{ route('register.show') }}" class="btn btn-secondary mb-3">Регистрация</a>
-        @endguest
         <li class="nav-item">
-          <a class="nav-link" href="{{ url('/') }}">Главная</a>
+          {{-- Кнопки для авторизованного пользователя --}}
+          @auth
+              <form action="{{ route('logout') }}" method="POST" class="d-inline">
+                  @csrf
+                  <button type="submit" class="btn btn-danger mb-3">Выйти</button>
+              </form>
+          @endauth
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{ url('/blog') }}">Блог</a>
+          {{-- Кнопки для гостя --}}
+          @guest
+            <a href="{{ route('login.show') }}" class="btn btn-primary mb-3">Войти</a>
+            <a href="{{ route('register.show') }}" class="btn btn-secondary mb-3">Регистрация</a>
+          @endguest
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{ url('/about') }}">О нас</a>
